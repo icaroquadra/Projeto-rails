55384f498e693916125500a55f76af0b67e950fa
danlei27 <danleisantos@hotmail.com>
2020-03-15 14:10:54 -0300

    relacionando usuario com lista de contatos, e usando o bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+  before_action :require_logged_in_user, only: [:edit, :update]
+
   def new
   	@user = User.new
   end
@@ -13,6 +16,18 @@ class UsersController < ApplicationController
   	end
   end
   
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      flash[:sucess] = 'Dados atualizados'
+      redirect_to contacts_url
+    else
+      render 'edit'
+    end
+  end
+
   private
     def user_params
     	params.require(:user).permit(:email, :name, :password, :password_confirmation)
