de50ee4f944871b4d08d03230f688d8e8b53cf84
danlei27 <danleisantos@hotmail.com>
2020-03-14 23:53:32 -0300

    adicionando autenticacao

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
 	  has_secure_password
+	  
 
 
 	  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -10,5 +11,9 @@ class User < ApplicationRecord
 	  validates :email, presence: true, length: { maximum: 255 },
 	                                    format: { with: VALID_EMAIL_REGEX },
 	                                    uniqueness: { case_sensitive: true }
+    
+   
+
+  
 
 end
